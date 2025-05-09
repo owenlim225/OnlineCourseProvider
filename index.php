06d298baf328a8c495873e2835c41ec868225c2f
Ellisia <christianjudevillaber@gmail.com>
2025-05-09 20:13:37 +0800

    Fix Mobile navbar toogle

--- index.php
+++ index.php
@@ -377,7 +377,7 @@ include("func/connections.php");
 <div id="preloader"></div>
 
 <!-- Vendor JS Files -->
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
 <script src="vendor/php-email-form/validate.js"></script>
 <script src="vendor/aos/aos.js"></script>
 <script src="vendor/swiper/swiper-bundle.min.js"></script>
